b9becd057c45f79b7366f23f2b66c2bf3b7fdff5
A01641937-alf <88739339+A01641937-alf@users.noreply.github.com>
2023-06-08 00:28:01 -0600

    bootstrap1

---
+++ hockey-app/src/componentes/NavBar/NavBar.js
@@ -0,0 +1,33 @@
+import React from "react";
+import img from '../../img/NHL-logo.png'
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous"/>
+
+function NavBar() {
+    return (
+      <>
+        <nav class="navbar bg-body-tertiary">
+            <div class="container">
+                <a class="navbar-brand" href="#">
+                <img src={img} alt="Bootstrap" width="30" height="24"/>
+                </a>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav justify-content-end">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">History</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            </nav>
+      </>
+    );
+  }
+  
+  export default Menu;
