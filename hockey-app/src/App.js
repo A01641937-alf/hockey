164f5d61f89bbeac8a3f5e5f9fd394e7273a1169
A01641937-alf <88739339+A01641937-alf@users.noreply.github.com>
2023-06-08 18:18:17 -0600

    prueba

--- hockey-app/src/App.js
+++ hockey-app/src/App.js
@@ -1,20 +1,22 @@
 import './App.css';
-import VideoPlayer  from './componentes/Video/Video';
+import Video  from './componentes/Video/Video';
 import Peleas from './componentes/Tarjeta/Prueba_api';
 import Footer from './componentes/Footer/Footer';
 import NavBar from './componentes/NavBar/NavBar';
 import Home from './componentes/Home/Home';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  NavLink
+} from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      <NavBar/>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/" element={<Peleas />} />
-        <Route path="/" element={<Video />} />
-      </Routes>
-      <Footer/>
+      <h1>okey........</h1>
     </div>
   );
 }
