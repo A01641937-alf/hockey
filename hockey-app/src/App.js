25ad6c6986ce76df6ff7d4b0d92f15a1edac483b
Erick Eduardo Orozco Reyes <A01198247@tec.mx>
2023-06-08 18:39:04 -0600

    guardado1

--- hockey-app/src/App.js
+++ hockey-app/src/App.js
@@ -1,19 +1,27 @@
 import './App.css';
-import VideoPlayer  from './componentes/Video/Video';
+import Video  from './componentes/Video/Video';
 import Peleas from './componentes/Tarjeta/Prueba_api';
 import Footer from './componentes/Footer/Footer';
 import NavBar from './componentes/NavBar/NavBar';
 import Home from './componentes/Home/Home';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  NavLink
+} from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
-      <Routes>
+      <Router>
         <Route path="/" element={<Home />} />
         <Route path="/" element={<Peleas />} />
         <Route path="/" element={<Video />} />
-      </Routes>
+      </Router>
       <Footer/>
     </div>
   );
